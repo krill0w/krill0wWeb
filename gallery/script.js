28e9196e05f48f458151606a467a575cc83f6a18
krill0w <52582629+krill0w@users.noreply.github.com>
2025-03-11 20:56:22 +0000

    renamed loading files and added to gallery to test sizing

--- gallery/script.js
+++ gallery/script.js
@@ -1,9 +1,9 @@
 const images = [
     { id: 1, url: 'https://www.krill0w.garden/assets/images/constructionCat.jpg', description: 'Image 1' },
     { id: 2, url: 'https://www.krill0w.garden/assets/images/krill0wTabIcon.png', description: 'Image 2' },
-    { id: 3, url: 'images/image3.jpg', description: 'Image 3' },
-    { id: 4, url: 'images/image4.jpg', description: 'Image 4' },
-    { id: 5, url: 'images/image5.jpg', description: 'Image 5' },
+    { id: 3, url: 'https://www.krill0w.garden/assets/images/loading.gif', description: 'Image 3' },
+    { id: 4, url: 'https://www.krill0w.garden/assets/images/loading100xBigger.gif', description: 'Image 4' },
+    { id: 5, url: 'https://www.krill0w.garden/assets/images/loading1000xBiggerAndGreen.gif', description: 'Image 5' },
     { id: 6, url: 'images/image6.jpg', description: 'Image 6' },
 ];
 
